9fed731f1a2ef8605a7085f925bafbc445327963
Larksel <lemuelgames@outlook.com>
2025-01-05 23:30:20 -0300

    :sparkles: feat: controller de vendas criado

--- GerenciadorVendas.WebAPI/Controllers/VendasController.cs
+++ GerenciadorVendas.WebAPI/Controllers/VendasController.cs
@@ -1,11 +1,101 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using GerenciadorVendas.WebAPI.Context;
+using GerenciadorVendas.WebAPI.Models;
 
 namespace GerenciadorVendas.WebAPI.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class VendasController : ControllerBase
-	{
-	}
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VendasController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public VendasController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Venda>>> GetVendas()
+        {
+            return await _context.Vendas.ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Venda>> GetVenda(int id)
+        {
+            var venda = await _context.Vendas.FindAsync(id);
+
+            if (venda == null)
+            {
+                return NotFound();
+            }
+
+            return venda;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutVenda(int id, Venda venda)
+        {
+            if (id != venda.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(venda).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!VendaExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Venda>> PostVenda(Venda venda)
+        {
+            _context.Vendas.Add(venda);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetVenda), new { id = venda.Id }, venda);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteVenda(int id)
+        {
+            var venda = await _context.Vendas.FindAsync(id);
+            if (venda == null)
+            {
+                return NotFound();
+            }
+
+            _context.Vendas.Remove(venda);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool VendaExists(int id)
+        {
+            return _context.Vendas.Any(e => e.Id == id);
+        }
+    }
 }

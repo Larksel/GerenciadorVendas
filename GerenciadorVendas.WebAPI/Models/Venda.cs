cc9294b52c6e1ee091723d88d830f85232490269
Larksel <lemuelgames@outlook.com>
2025-01-05 22:52:02 -0300

    :sparkles: feat: model Venda criada

---
+++ GerenciadorVendas.WebAPI/Models/Venda.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GerenciadorVendas.WebAPI.Models;
+
+[Table("venda")]
+public class Venda
+{
+	[Column("id")]
+	public int Id { get; set; }
+
+	[Column("produto")]
+	public string Produto { get; set; }
+
+	[Column("quantidade")]
+	public int Quantidade { get; set; }
+	
+	[Column("valorunit")]
+	public double ValorUnit { get; set; }
+
+	[Column("valortotal")]
+	public double ValorTotal { get; set; }
+
+	public Venda(int id, string produto, int quantidade, double valorUnitario)
+	{
+		Id = id;
+		Produto = produto;
+		Quantidade = quantidade;
+		ValorUnit = valorUnitario;
+		ValorTotal = valorUnitario * quantidade;
+	}
+}
